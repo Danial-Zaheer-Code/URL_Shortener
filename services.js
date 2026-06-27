77b1c45450f8bc56d8155a535d5c9509f1275c44
Danial-Zaheer-Code <danialzaheer80@gmail.com>
2026-06-27 15:06:23 +0500

    Changes the inmemory DB method to use Postgres locahost server instead.

--- services.js
+++ services.js
@@ -1,27 +1,33 @@
+import { pool } from "./dbConfig.js";
+
 const db = []
 
 
-export function createRow(url){
-    let id = db.length == 0 ? 1 : Math.max(db.map(item => item.id)) + 1;
-    db.push({
-        id: id,
-        base62: "",
-        url: url
-    })
+export async function createRow(url){
+    const result = await pool.query(`
+        INSERT INTO url_mapper (url)
+        VALUES ($1) RETURNING id   
+    `,[url]);
 
-    return id;
+    if(result.rowCount <= 0){
+        throw new Error("Create Row Failed to Insert");
+    }
+
+    return result.rows[0].id;
 }
 
-export function updateBase62(id, base62){
-    db.map(item => {
-        if(item.id == id){
-            item.base62 = base62;
-        }
-    })
+export async function updateBase62(id, shortened_url){
+    const result = await pool.query(`
+        UPDATE url_mapper
+        SET shortened_url = $1
+        WHERE id = $2
+    `,[shortened_url,id]); 
 }
 
-export function getOriginalLink(convertedString){
-    const result = db.filter(item => item.base62 == convertedString);
+export async function getOriginalLink(shortened_url){
+    const {rows} = await pool.query(`
+        SELECT url FROM url_mapper WHERE shortened_url = $1
+    `,[shortened_url]);
     
-    return result.length == 0 ? null : result[0].url;
+    return rows;
 }
